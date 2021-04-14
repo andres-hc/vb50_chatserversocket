232fcd345e1ed96355a932db603712c2ae021879
Andres <andres.ahc@gmail.com>
2021-04-14 19:45:31 -0400

    Generar el cliente socket

--- ChatServerApp/ClienteSocketApp/Program.cs
+++ ChatServerApp/ClienteSocketApp/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ClienteSocketApp.Comunicacion;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,36 @@ namespace ClienteSocketApp
     {
         static void Main(string[] args)
         {
+            string ip = ConfigurationManager.AppSettings["ip"];
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Iniciando conexion a servidor {0} en el puerto {1}", ip, puerto);
+            ClienteSocket clienteSocket = new ClienteSocket(puerto, ip);
+
+            if (clienteSocket.Conectar())
+            {
+                //Protocolo de comunicacion
+                string mensaje = "";
+
+                while(mensaje.ToLower() != "chao")
+                {
+                    Console.WriteLine("Ingrese mensaje:");
+                    mensaje = Console.ReadLine().Trim();
+                    clienteSocket.Escribir(mensaje);
+                    if(mensaje.ToLower() != "chao")
+                    {
+                        mensaje = clienteSocket.Leer();
+                        Console.WriteLine("S:{0}", mensaje);
+                    }
+                }
+                clienteSocket.Desconectar();
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error al conectar al servidor");
+            }
         }
     }
 }
